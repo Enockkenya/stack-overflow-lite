5ab037c1e878a42be33741b2929cccf953048211
enock kenya <enockomondike@gmail.com>
2018-08-20 03:13:45 +0300

    ft-user-able-post-question-159778493

--- app/app.py
+++ app/app.py
@@ -7,6 +7,26 @@ from Instance.config import app_config
 def create_app(config_name):
 
     app = FlaskAPI(__name__, instance_relative_config=True)
+    
+    users = []
+    questions = []
+    answers = []
+
+
+ 
+                
+
+    @app.route('/api/v1/questions', methods=['POST'])
+    def question():
+        # Post a question
+        question = {'id': len(questions)+1,
+            'Questions': request.json.get('Question'),
+            'Date posted': request.json.get('Date posted')
+        }
+        questions.append(question)
+        return jsonify({'Message': "Question successfully created"} ,{'Questions': questions}), 201
+
+
 
 
     return app
